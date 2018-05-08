6b45294fef3a491250ef4be62597c83035de0f6f
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2018-05-08 16:49:44 +0200

    Add react redux

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import  axios from '../../../axios-order'
+import { connect } from 'react-redux'
 
 import Button from '../../../components/UI/Button/Button'
 import classes from './ContactData.css'
@@ -107,7 +108,7 @@ class ContactData extends Component {
       formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value
     }
     const order = {
-      ingredients: this.props.ingredients,
+      ingredients: this.props.ings,
       price: this.props.price,
       orderData: formData
     }
@@ -200,4 +201,11 @@ class ContactData extends Component {
   }
 }
 
-export default withErrorHandler(ContactData, axios)
+const mapStateToProps = state => {
+  return {
+    ings: state.ingredients,
+    price: state.totalPrice
+  }
+}
+
+export default connect(mapStateToProps)(withErrorHandler(ContactData, axios))
